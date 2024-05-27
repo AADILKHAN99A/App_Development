52ce56646a94961c1a7e3ac8e2c3d849f0c1fac9
Aadil Khan <aadilkhan99a@gmail.com>
2024-05-27 11:12:33 +0530

    Apk added

---
+++ smart_chat/android/app/src/main/kotlin/com/example/smart_chat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.smart_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- the_expenses/lib/main.dart
+++ the_expenses/lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'The Expenses',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
